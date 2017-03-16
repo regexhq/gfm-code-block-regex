7fb9af78ae31af1ce4032b0c92ea8a74ee078320
jonschlinkert <jon.schlinkert@sellside.com>
2017-03-16 11:35:48 -0400

    only spaces or tabs before backticks

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /*!
- * gfm-code-block-regex <https://github.com/jonschlinkert/gfm-code-block-regex>
+ * gfm-code-block-regex <https://github.com/regexhq/gfm-code-block-regex>
  *
  * Copyright (c) 2014, 2017, Jon Schlinkert.
  * Released under the MIT License.
@@ -8,5 +8,5 @@
 'use strict';
 
 module.exports = function() {
-  return /^((`{3,4})([^\n]*)([\s\S]+? *)(\2))/gm;
+  return /^(([ \t]*`{3,4})([^\n]*)([\s\S]+?)(^[ \t]*\2))/gm;
 };
