5f7719ee6d5eafa4ce1270b9ce06403455ec1abf
jonschlinkert <jon.schlinkert@sellside.com>
2017-03-16 11:36:03 -0400

    and and update tests, lint

--- test.js
+++ test.js
@@ -12,21 +12,101 @@ var re = require('./');
 
 describe('code block', function () {
   it('should return the code blocks from the fixture.', function () {
-  	var match = re().exec('foo\n```bash\nnpm i helper-loader --save-dev\n```\nbar');
+  	var match = re().exec('foo\n```sh\nnpm install micromatch --save-dev\n```\nbar');
     assert(Array.isArray(match));
     assert.deepEqual(match.slice(), [
-      '```bash\nnpm i helper-loader --save-dev\n```',
-      '```bash\nnpm i helper-loader --save-dev\n```',
+      '```sh\nnpm install micromatch --save-dev\n```',
+      '```sh\nnpm install micromatch --save-dev\n```',
       '```',
-      'bash',
-      '\nnpm i helper-loader --save-dev\n',
+      'sh',
+      '\nnpm install micromatch --save-dev\n',
       '```'
     ]);
   });
 
   it('should get the language from a code block.', function () {
-    var match = re().exec('foo\n```bash\nnpm i helper-loader --save-dev\n```\nbar');
-    assert.equal(match[3], 'bash');
+    var match = re().exec('foo\n```sh\nnpm install micromatch --save-dev\n```\nbar');
+    assert.equal(match[3], 'sh');
+  });
+
+  it('should work when gfm blocks are in code comments', function () {
+    var match = re().exec([
+      'Some text',
+      '',
+      '```js',
+      '/**',
+      ' * This is a comment with',
+      ' * several lines of text.',
+      ' *',
+      ' * An example',
+      ' *',
+      ' * ```js',
+      ' * var foo = bar;',
+      ' * var foo = bar;',
+      ' * var foo = bar;',
+      ' * ```',
+      ' */',
+      '```',
+      'More text',
+    ].join('\n'));
+
+    assert.equal(match[1], [
+      '```js',
+      '/**',
+      ' * This is a comment with',
+      ' * several lines of text.',
+      ' *',
+      ' * An example',
+      ' *',
+      ' * ```js',
+      ' * var foo = bar;',
+      ' * var foo = bar;',
+      ' * var foo = bar;',
+      ' * ```',
+      ' */',
+      '```',
+    ].join('\n'));
+    assert.equal(match[3], 'js');
+  });
+
+  it('should work with indented gfm blocks', function () {
+    var match = re().exec([
+      'Some text',
+      '',
+      '  ```js',
+      '  /**',
+      '   * This is a comment with',
+      '   * several lines of text.',
+      '   *',
+      '   * An example',
+      '   *',
+      '   * ```js',
+      '   * var foo = bar;',
+      '   * var foo = bar;',
+      '   * var foo = bar;',
+      '   * ```',
+      '   */',
+      '  ```',
+      'More text',
+    ].join('\n'));
+
+    assert.equal(match[1], [
+      '  ```js',
+      '  /**',
+      '   * This is a comment with',
+      '   * several lines of text.',
+      '   *',
+      '   * An example',
+      '   *',
+      '   * ```js',
+      '   * var foo = bar;',
+      '   * var foo = bar;',
+      '   * var foo = bar;',
+      '   * ```',
+      '   */',
+      '  ```',
+    ].join('\n'));
+    assert.equal(match[3], 'js');
   });
 
   it('should get a code block', function () {
